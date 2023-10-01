56e9d82496436b6101d0d11bed768e0281df22f4
Aaron Dodd <me@aaronrdodd.com>
2023-10-01 21:04:43 +0100

    init: hardened firefox

---
+++ home/aaron/_common/desktop/firefox.nix
@@ -0,0 +1,87 @@
+{ pkgs, ... }:
+let
+  inherit (pkgs.nur.repos.rycee) firefox-addons;
+in
+{
+  programs.firefox = {
+    enable = true;
+    package = pkgs.unstable.firefox-esr.override {
+      cfg = {
+        smartCardSupport = true;
+      };
+      extraPolicies = {
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = true;
+        FirefoxHome = {
+          Pocket = false;
+          Snippets = false;
+        };
+        OfferToSaveLogins = false;
+        UserMessaging = {
+          SkipOnboarding = true;
+          ExtensionRecommendations = false;
+          MoreFromMozilla = false;
+        };
+      };
+    };
+    profiles = {
+      aaron = {
+        isDefault = true;
+        bookmarks = { };
+        extensions = with firefox-addons; [
+          header-editor
+          multi-account-containers
+          redirector
+          skip-redirect
+          ublock-origin
+        ];
+        settings = {
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.default.sites" = "";
+          "browser.newtabpage.enabled" = false;
+          "browser.startup.homepage" = "https://search.brave.com";
+          "browser.startup.page" = 0;
+
+          "geo.provider.network.url" = "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%";
+          "geo.provider.ms-windows-location" = false;
+          "geo.provider.use_corelocation" = false;
+          "geo.provider.use_gpsd" = false;
+          "geo.provider.use_geoclue" = false;
+
+          "extensions.getAddons.showPane" = false;
+          "extensions.htmlaboutaddons.recommendations.enabled" = false;
+          "browser.discovery.enabled" = false;
+
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.server" = "data:,";
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.coverage.opt-out" = true;
+          "toolkit.coverage.opt-out" = true;
+          "toolkit.coverage.endpoint.base" = "";
+          "browser.ping-centre.telemetry" = false;
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+
+          "app.shield.optoutstudies.enabled" = false;
+          "app.normandy.enabled" = false;
+          "app.normandy.api_url" = "";
+
+          "dom.security.https_only_mode" = true;
+          "privacy.trackingprotection.enabled" = true;
+        };
+      };
+    };
+  };
+}
