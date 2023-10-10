3416c9eedaf4942cf5b9f26e5def28bb340faf74
Aaron Dodd <me@aaronrdodd.com>
2023-10-10 17:39:19 +0100

    fix: plasma tweaks

--- nixos/_common/desktop/plasma.nix
+++ nixos/_common/desktop/plasma.nix
@@ -1,12 +1,25 @@
-{ pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.packagekit.enable = true;
+
+  # Enable dconf
+  programs.dconf.enable = true;
 
   # Enable SSH askpass
   programs.ssh.askPassword = lib.mkForce "${pkgs.libsForQt5.ksshaskpass.out}/bin/ksshaskpass";
+
+  environment.systemPackages = with pkgs;
+    if config.services.flatpak.enable
+    then [
+      libsForQt5.discover
+      libsForQt5.packagekit-qt
+      packagekit
+    ]
+    else [ ];
 }
 
