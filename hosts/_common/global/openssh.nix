8b46cedeb6cbe6d1856f27b16baa28ac4922966a
Aaron Dodd <aarondodd@protonmail.com>
2024-02-28 17:24:14 +0000

    fix: update insecure authorizedKeysFiles

--- hosts/_common/global/openssh.nix
+++ hosts/_common/global/openssh.nix
@@ -27,5 +27,10 @@
   programs.ssh.startAgent = true;
 
   # Passwordless sudo when SSH'ing with keys
-  security.pam.sshAgentAuth.enable = true;
+  security.pam.sshAgentAuth = {
+    enable = true;
+    authorizedKeysFiles = [
+      "/etc/ssh/authorized_keys.d/%u"
+    ];
+  };
 }
